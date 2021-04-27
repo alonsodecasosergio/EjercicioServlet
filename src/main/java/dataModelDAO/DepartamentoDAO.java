5a33a5d928da167003a21ca572d1757659a4443f
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-27 19:18:24 +0200

    Añadido de los ficheros de configuracion y mapeo de hibernate juntos a las clases de entidades y controladores DAO los cuales se utilizaron en un ejercicio anterior.

---
+++ src/main/java/dataModelDAO/DepartamentoDAO.java
@@ -0,0 +1,64 @@
+package dataModelDAO;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+
+import dataModelEntities.Departamento;
+import servlet.MostrarDepartamentos;
+
+public abstract class DepartamentoDAO {
+	
+	private static Logger logger = LogManager.getLogger(MostrarDepartamentos.class);
+	
+	//INSERTA UN DEPARTAMENTO EN LA BASE DE DATOS
+	public static void inserDepartamento(Session s, Departamento depar) {
+		s.save(depar);
+		logger.debug("Inserccion del departamento " + depar.toString());
+	}
+	
+	//DEVUELVE UNA LISTA CON TODOS LOS DEPARTAMENTOS
+	public static List<Departamento> getAllDepartamento(Session s){
+		String hQuery = "from Departamento";
+		List<Departamento> departamentoList = s.createQuery(hQuery, Departamento.class).list();
+		
+		return departamentoList;
+	}
+	
+	//DEVUELVE EL DEPARTAMENTO SEGUN SU CODIGO
+	public static Departamento getDepartamento(Session s, int departamentoId) {
+		String hQuery = " from Departamento d where d.codigo = :codigo";
+		Departamento departamento = s.createQuery(hQuery, Departamento.class)
+											.setParameter("codigo", departamentoId)
+											.setMaxResults(1)
+											.uniqueResult();
+		return departamento;
+	}
+	
+	//BORRA UN DEPARTAMENTO SEGUN SU CODIGO DE DEPARTAMENTO
+	public static void deleteDepartamento(Session s, int departamentoId) {
+		
+		Departamento depar = s.get(Departamento.class, departamentoId);
+		
+		s.delete(depar);
+		
+		logger.debug("Borrado del departamento: " + depar.toString());
+		
+	}
+	
+	//ACTUALIZA LOS DATOS DEL DEPARTAMENTO 
+	public static void updateDepartamento(Session s, int departamentoId, Departamento deparActualizado) {
+		
+		Departamento depar = s.get(Departamento.class, departamentoId);
+		
+		depar.setCod_responsable(deparActualizado.getCod_responsable());
+		depar.setNombre(deparActualizado.getNombre());
+		
+		s.update(depar);
+		
+		logger.debug("Actualizacion del departamento: " + depar.toString());
+	}
+
+}
---
+++ src/main/java/dataModelDAO/EmpleadoDAO.java
@@ -0,0 +1,73 @@
+package dataModelDAO;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import dataModelEntities.Empleado;
+import servlet.MostrarDepartamentos;
+
+public abstract class EmpleadoDAO {
+	
+	private static Logger logger = LogManager.getLogger(MostrarDepartamentos.class);
+	
+	//METODO EL CUAL INSERTARA EL EMPLEADO EN LA BASE DE DATOS
+	public static void insertEmpleados(Session s, Empleado empleado) {
+		
+		s.save(empleado);
+		logger.debug("Inserccion del departamento " + empleado.toString());
+	}
+	
+	//OBTIENE UNA LISTA DE TODOS LOS EMPLEADOS
+	public static List<Empleado> getAllEmpleado(Session s){
+		String hQuery = "from Empleado";
+		List<Empleado> empleadoList = s.createQuery(hQuery, Empleado.class).list();
+		
+		return empleadoList;
+	}
+	
+	//OBTIENE EL EMPLEADO SEGUN EL ID PASADO COMO PARAMETRO
+	public static Empleado getEmpleado(Session s, int empleadoId) {
+		String hQuery = " from Empleado e where e.codigo = :codigo";
+		Empleado empleado = s.createQuery(hQuery, Empleado.class)
+									.setParameter("codigo", empleadoId)
+									.setMaxResults(1)
+									.uniqueResult();
+		return empleado;
+	}
+	
+	//BORRA UN EMPLEADO SEGUN SU CODIGO DE EMPLEADO
+	public static void deleteEmpleado(Session s, int empleadoId) {
+		
+		Empleado empleado = s.get(Empleado.class, empleadoId);
+		
+		s.delete(empleado);
+		
+		logger.debug("Borrado del departamento " + empleado.toString());
+
+		
+	}
+	
+	//ACTUALIZA EL EMPLEADO CAMBIANDO SUS DATOS POR LOS DATOS NUEVOS
+	public static void updateEmpleado(Session s, int empleadoId, Empleado empleActualizado) {
+		
+		Empleado empleado = s.get(Empleado.class, empleadoId);
+		
+		empleado.setNombre(empleActualizado.getNombre());
+		empleado.setApellido1(empleActualizado.getApellido1());
+		empleado.setApellido2(empleActualizado.getApellido2());
+		empleado.setDireccion(empleActualizado.getDireccion());
+		empleado.setFechaNacimiento(empleActualizado.getFechaNacimiento());
+		empleado.setLugarNacimiento(empleActualizado.getLugarNacimiento());
+		empleado.setPuesto(empleActualizado.getPuesto());
+		empleado.setTelefono(empleActualizado.getTelefono());
+		empleado.setCod_departamento(empleActualizado.getCod_departamento());
+		
+		s.update(empleado);
+		
+		logger.debug("Actualizacion del departamento " + empleado.toString());
+
+	}
+
+}
