006dc3842ab04ca2a9ec437d5a638329aaddbbdc
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-28 16:25:42 +0200

    Añadido de los metodos que imprimiran la tabla correspondiente por pantalla segun el parametro obtenido en la URL si el parametro es empleado aparecera la tabla de empleados y si el parametro es departamento aparecera la tabla de departamentos

--- src/main/java/servlet/MostrarDatos.java
+++ src/main/java/servlet/MostrarDatos.java
@@ -1,6 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +12,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import dataModelDAO.DepartamentoDAO;
+import dataModelDAO.EmpleadoDAO;
+import dataModelEntities.Departamento;
+import dataModelEntities.Empleado;
+import dataModelUtils.HibernateUtil;
+
 /**
  * Servlet implementation class MostrarDatos
  */
 @WebServlet("/MostrarDatos")
 public class MostrarDatos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static Logger logger = LogManager.getLogger(MostrarDepartamentos.class);
+	
+	static SessionFactory sessionFactory;
+	static Session session;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +48,10 @@ public class MostrarDatos extends HttpServlet {
 	 */
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
+		
+		logger.info("%1$s: >>>>>> Main execution started. MOSTRAR DATOS");
+		
+		session = HibernateUtil.getSessionFactory().openSession();
 	}
 
 	/**
@@ -35,7 +59,27 @@ public class MostrarDatos extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String parameter = request.getParameter("table");
+		
+		logger.debug("Peticion de todos los departamentos");
+		PrintWriter out = response.getWriter();
+		if(parameter.equals("empleado")) {
+			
+			logger.debug("Peticion de todos los empleados");
+			List<Empleado> listaEmpleado = EmpleadoDAO.getAllEmpleado(session);
+			logger.debug("Escritura de los empleados en la tabla");
+			printResponseEmpleados(out, listaEmpleado);
+			
+		}else if(parameter.equals("departamento")) {
+			
+			logger.debug("Peticion de todos los departamentos");
+			List<Departamento> listaDepartamento = DepartamentoDAO.getAllDepartamento(session);
+			logger.debug("Escritura de los departamentos en la tabla");
+			printResponseDepartamentos(out, listaDepartamento);
+			
+		}
+		out.close();
 	}
 
 	/**
@@ -44,6 +88,89 @@ public class MostrarDatos extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+	}
+	
+	private PrintWriter printResponseDepartamentos(PrintWriter out, List<Departamento> listaDepartamento) {
+		
+		PrintWriter res = out;
+		
+		res.println("<html>");
+		res.println("<title>Ejercicio Serverlet | Sergio Alonso</title>");
+		res.println("<body>");
+		res.println("<h1>TABLA DEPARTAMENTOS</h1>");		
+		res.println("<table border=\"2\">");
+		res.println("<tr>");
+		res.println("<td> CODIGO </td>");
+		res.println("<td> NOMBRE </td>");
+		res.println("<td> CODIGO RESPONSABLE </td>");
+		res.println("</tr>");
+		for(int i = 0; i < listaDepartamento.size(); i++) {
+			
+			Departamento depar = listaDepartamento.get(i);
+			res.println("<tr>");
+			res.println("<td>" + depar.getCodigo() + "</td>");
+			res.println("<td>" + depar.getNombre() + "</td>");
+			res.println("<td>" + depar.getCod_responsable() + "</td>");
+			res.println("</tr>");
+		}
+		res.println("</table>");
+		res.println("</body>");
+		res.println("</html>");
+		
+		return res;
+	}
+	
+	private PrintWriter printResponseEmpleados(PrintWriter out, List<Empleado> listaEmpleado) {
+		
+		PrintWriter res = out;
+		
+		res.println("<html>");
+		res.println("<title>Ejercicio Serverlet | Sergio Alonso</title>");
+		res.println("<body>");
+		res.println("<h1>TABLA EMPLEADOS</h1>");		
+		res.println("<table border=\"2\">");
+		res.println("<tr>");
+		res.println("<td> CODIGO </td>");
+		res.println("<td> NOMBRE </td>");
+		res.println("<td> PRIMER APELLIDO </td>");
+		res.println("<td> SEGUNDO APELLIDO </td>");
+		res.println("<td> LUGAR DE NACIMIENTO </td>");
+		res.println("<td> FECHA DE NACIMEINTO </td>");
+		res.println("<td> DIRECCION </td>");
+		res.println("<td> TELEFONO </td>");
+		res.println("<td> PUESTO </td>");
+		res.println("<td> CODIGO DE DEPARTAMENTO </td>");
+		
+		res.println("</tr>");
+		for(int i = 0; i < listaEmpleado.size(); i++) {
+			
+			Empleado emple = listaEmpleado.get(i);
+			
+			res.println("<tr>");
+			res.println("<td>" + emple.getCodigo() + "</td>");
+			res.println("<td>" + emple.getNombre() + "</td>");
+			res.println("<td>" + emple.getApellido1() + "</td>");
+			res.println("<td>" + emple.getApellido2() + "</td>");
+			res.println("<td>" + emple.getLugarNacimiento() + "</td>");
+			
+			String fNacimiento = "";
+			for(int j = 0; j < emple.getFechaNacimiento().length; j++)  {
+				fNacimiento += emple.getFechaNacimiento()[j];
+			}
+			
+			res.println("<td>" + fNacimiento + "</td>");
+			res.println("<td>" + emple.getDireccion()+ "</td>");
+			res.println("<td>" + emple.getTelefono() + "</td>");
+			res.println("<td>" + emple.getPuesto() + "</td>");
+			res.println("<td>" + emple.getCod_departamento() + "</td>");
+			
+			res.println("</tr>");
+		}
+		res.println("</table>");
+		res.println("</body>");
+		res.println("</html>");
+		
+		return res;
 	}
 
 }
